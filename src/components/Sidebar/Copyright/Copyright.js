56de7b88de6be51edc30fa9dffa131c1fd4d3c30
bkrm dahal <bkrm.dahal@gmail.com>
2020-09-19 20:34:07 +0545

    added newsletter

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -7,8 +7,13 @@ type Props = {
 };
 
 const Copyright = ({ copyright }: Props) => (
+  <div>
+    <div className={styles['copyright']}>
+  <iframe src="https://bkrm.substack.com/embed" width="290" height="250" styles="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
+  </div>
   <div className={styles['copyright']}>
     {copyright}
+  </div>
   </div>
 );
 
